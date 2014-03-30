ca8b05d36e55f439548d2d5d518ff73ea258c7ce
dludaya <dludaya.cs@gmail.com>
2014-03-30 18:16:23 -0400

    SpringTest

---
+++ helloSpring/src/com/practice/helloSpring.java
@@ -0,0 +1,14 @@
+package com.practice;
+
+public class helloSpring {
+	  private String message;
+
+	   public void setMessage(String message){
+	      this.message  = message;
+	   }
+
+	   public void getMessage(){
+	      System.out.println("Your Message : " + message);
+	   }
+
+}
